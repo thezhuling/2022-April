78904fe7e7bd6fc4c4a85ddb62862d196641929f
zhuling <thezhuling@gmail.com>
2022-04-27 21:30:45 +0800

    Spring Cloud Alibaba add config

--- config-service/src/main/java/com/zhuling/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/zhuling/config/ConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.zhuling.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
--- config-service/src/main/java/com/zhuling/config/controller/ConfigDemoController.java
+++ config-service/src/main/java/com/zhuling/config/controller/ConfigDemoController.java
@@ -1,4 +1,14 @@
 package com.zhuling.config.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
 public class ConfigDemoController {
+    @GetMapping("config")
+    public String configDemo() {
+        return "ok";
+    }
 }
