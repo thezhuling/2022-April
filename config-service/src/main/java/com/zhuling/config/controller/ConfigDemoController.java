63ce63f9c56e4feb2648cce92844e5d0dd3729a6
zhuling <thezhuling@gmail.com>
2022-04-28 00:34:16 +0800

    test:git-command

--- config-service/src/main/java/com/zhuling/config/controller/ConfigDemoController.java
+++ config-service/src/main/java/com/zhuling/config/controller/ConfigDemoController.java
@@ -1,14 +1,20 @@
 package com.zhuling.config.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RefreshScope
 public class ConfigDemoController {
-    @GetMapping("config")
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("config/info")
     public String configDemo() {
-        return "ok";
+        return configInfo;
     }
 }
