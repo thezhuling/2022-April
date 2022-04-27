84d47e0b846524b5b3880c7645f228e3191edcc9
zhuling <thezhuling@gmail.com>
2022-04-27 16:13:28 +0800

    Spring Cloud Alibaba init

--- provider-service/src/main/java/com/zhuling/provider/ProviderServiceApplication.java
+++ provider-service/src/main/java/com/zhuling/provider/ProviderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.zhuling.provider.provider;
+package com.zhuling.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- provider-service/src/test/java/com/zhuling/provider/ProviderServiceApplicationTests.java
+++ provider-service/src/test/java/com/zhuling/provider/ProviderServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zhuling.provider.provider;
+package com.zhuling.provider;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
